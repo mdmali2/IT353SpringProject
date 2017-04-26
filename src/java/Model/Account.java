24f84356cce89ed3bc7f1228d7aba1d1d24d4964
balenciaga <IT353S741@AD.ILSTU.EDU>
2017-04-25 19:04:28 -0500

    Apply/account

--- src/java/Model/Account.java
+++ src/java/Model/Account.java
@@ -17,6 +17,15 @@ public class Account {
     private String email;
     private int isAdmin;
     
+    //applytoschool strings
+    private String gpa;
+    private String activites;
+    private String desireduni;
+    private String highscool;
+    private String sat;
+    private String act;
+    
+    
     public Account() {
        
     }
@@ -27,6 +36,13 @@ public class Account {
         this.email = email;
         this.password = pass;
         this.isAdmin = 0;
+        this.act=act;
+        this.sat=sat;
+        this.highscool=highscool;
+        this.desireduni=desireduni;
+        this.activites=activites;
+        this.gpa=gpa;
+                
     }
     /**
      * @return the password
@@ -130,6 +146,125 @@ public class Account {
 
     public String getUni() {
         return uni;
+    }
+
+    /**
+     * @return the gpa
+     */
+    public String getGpa() {
+        return gpa;
+    }
+
+    /**
+     * @param gpa the gpa to set
+     */
+    public void setGpa(String gpa) {
+        this.gpa = gpa;
+    }
+
+    /**
+     * @return the activites
+     */
+    public String getActivites() {
+        return activites;
+    }
+
+    /**
+     * @param activites the activites to set
+     */
+    public void setActivites(String activites) {
+        this.activites = activites;
+    }
+
+    /**
+     * @return the desireduni
+     */
+    public String getDesireduni() {
+        return desireduni;
+    }
+
+    /**
+     * @param desireduni the desireduni to set
+     */
+    public void setDesireduni(String desireduni) {
+        this.desireduni = desireduni;
+    }
+
+    /**
+     * @return the highscool
+     */
+    public String getHighscool() {
+        return highscool;
+    }
+
+    /**
+     * @param highscool the highscool to set
+     */
+    public void setHighscool(String highscool) {
+        this.highscool = highscool;
+    }
+
+    /**
+     * @return the sat
+     */
+    public String getSat() {
+        return sat;
+    }
+
+    /**
+     * @param sat the sat to set
+     */
+    public void setSat(String sat) {
+        this.sat = sat;
+    }
+
+    /**
+     * @return the act
+     */
+    public String getAct() {
+        return act;
+    }
+
+    /**
+     * @param act the act to set
+     */
+    public void setAct(String act) {
+        this.act = act;
+    }
+
+    /**
+     * @return the exampleData
+     */
+    public Map<String, String> getExampleData() {
+        return exampleData;
+    }
+
+    /**
+     * @param exampleData the exampleData to set
+     */
+    public void setExampleData(Map<String, String> exampleData) {
+        this.exampleData = exampleData;
+    }
+
+    /**
+     * @return the searchUni
+     */
+    public String getSearchUni() {
+        return searchUni;
+    }
+
+    /**
+     * @param searchUni the searchUni to set
+     */
+    public void setSearchUni(String searchUni) {
+        this.searchUni = searchUni;
+    }
+
+    /**
+     * @param uni the uni to set
+     */
+    public void setUni(String uni) {
+        this.uni = uni;
     }
 
 }
